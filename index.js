9465685709cd96090d3264fe83bcbd20d349b7d1
Ramesh <ramesh.gorekanti@gmail.com>
2024-11-18 16:15:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const port = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("hello from ramesh gorekanti");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<strong>please login at page</strong>");
+});
+
+app.get("/youtube", (req, res) => {
+  res.send("<h1>YouTube</h1>");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+});
